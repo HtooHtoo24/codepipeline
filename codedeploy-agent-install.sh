5e6597f62361d58d02549868a214ef26118012a2
HtooHtoo24 <htoo2agautomobile@gmail.com>
2020-12-19 15:39:47 +0630

    Update aws-codedeploy

--- codedeploy-agent-install.sh
+++ codedeploy-agent-install.sh
@@ -3,6 +3,6 @@
 sudo yum update -y 
 sudo yum install -y ruby wget 
 cd /opt 
-sudo wget https://aws-codedeploy-hha-ap-southeast-1.s3.amazonaws.com/latest/install
-chmod +x ./install 
-./install auto 
+sudo wget https://aws-codedeploy-ap-southeast-1.s3.amazonaws.com/latest/install
+sudo chmod +x ./install 
+sudo ./install auto 
